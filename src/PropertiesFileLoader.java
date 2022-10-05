b58450bce080c09a96fbe4373bbddf5e335624d8
Jan Niclas Ruppenthal <jan.rupp06@web.de>
2022-10-05 14:46:17 +0200

    Added PropertiesFileLoader and extractProperties()

---
+++ src/PropertiesFileLoader.java
@@ -0,0 +1,35 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class PropertiesFileLoader
+{
+
+    private static PropertiesFileLoader propertiesFileLoader = null;
+
+    private PropertiesFileLoader()
+    {
+
+    }
+
+
+    public static PropertiesFileLoader createPropertiesFileLoader()
+    {
+        if (propertiesFileLoader == null)
+        {
+            return new PropertiesFileLoader();
+        }
+
+        return propertiesFileLoader;
+    }
+
+    public Properties loadProperties(String filename) throws IOException
+    {
+        Properties properties = new Properties();
+        properties.load(new FileInputStream(filename));
+
+        return properties;
+    }
+
+}
