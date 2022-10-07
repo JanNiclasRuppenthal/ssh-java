ba10f092784d32c108eff9d30ac8e8c9e3b8767a
Jan Niclas Ruppenthal <jan.rupp06@web.de>
2022-10-07 15:21:22 +0200

    Refactoring

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,15 +3,18 @@ import com.jcraft.jsch.*;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.Properties;
-import java.util.zip.ZipFile;
 
 public class Main
 {
     private String username;
     private String password;
     private String IPadress;
+
+    private final String ABSOLUTPATHTOPYCHARMPROJECTS = "C:\\Users\\janru\\PycharmProjects";
+    private final String ZIPFILENAME = "TestCodeForCodeBubblesAR.zip";
+    private final String PROJECTNAME = "TestCodeForCodeBubblesAR";
+    private final String DESTINATION = "/home/jnruppenthal/";
 
     public static void main(String[] args)
     {
@@ -21,10 +24,14 @@ public class Main
 
         try
         {
-            zipTheFile();
+            main.zipTheFile();
         } catch (IOException e)
         {
             throw new RuntimeException(e);
+        }
+        finally
+        {
+            System.out.println("Zipping Process Completed...");
         }
 
         try
@@ -49,35 +56,9 @@ public class Main
         {
             throw new RuntimeException(e);
         }
-
-        // Thread does not end here?
-//        System.exit(0);
-    }
-
-    private static void zipTheFile() throws IOException
-    {
-        String[] command = {"powershell", "/C", "cd ..\\..\\PycharmProjects ; zip -r TestCodeForCodeBubblesAR.zip TestCodeForCodeBubblesAR"};
-        ProcessBuilder probuilder = new ProcessBuilder( command );
-
-        Process process = probuilder.start();
-
-        //Read out dir output
-        InputStream is = process.getInputStream();
-        InputStreamReader isr = new InputStreamReader(is);
-        BufferedReader br = new BufferedReader(isr);
-        String line;
-        System.out.printf("Output of running %s is:\n",
-                Arrays.toString(command));
-        while ((line = br.readLine()) != null) {
-            System.out.println(line);
-        }
-
-        //Wait to get exit value
-        try {
-            int exitValue = process.waitFor();
-            System.out.println("\n\nExit Value is " + exitValue);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        finally
+        {
+            System.out.println("Running Python Script Completed...");
         }
     }
 
@@ -99,6 +80,38 @@ public class Main
         this.IPadress = properties.getProperty("IPadress");
     }
 
+    private void zipTheFile() throws IOException
+    {
+        StringBuilder commandBuilder = new StringBuilder()
+                .append("cd " + this.ABSOLUTPATHTOPYCHARMPROJECTS)
+                .append(" ; ")
+                .append("zip -r " + this.ZIPFILENAME + " " + this.PROJECTNAME);
+        String[] command = {"powershell", "/C", commandBuilder.toString()};
+        ProcessBuilder probuilder = new ProcessBuilder( command );
+
+        Process process = probuilder.start();
+
+        InputStream is = process.getInputStream();
+        InputStreamReader isr = new InputStreamReader(is);
+        BufferedReader br = new BufferedReader(isr);
+        String line;
+        System.out.printf("Output of running %s is:\n",
+                Arrays.toString(command));
+        while ((line = br.readLine()) != null)
+        {
+            System.out.println(line);
+        }
+
+        //Wait to get exit value
+        try
+        {
+            int exitValue = process.waitFor();
+            System.out.println("\n\nExit Value is " + exitValue);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
     private void sftpToRemoteMachine() throws JSchException, FileNotFoundException, SftpException
     {
         JSch jsch = new JSch();
@@ -117,14 +130,14 @@ public class Main
         ChannelSftp channelSftp = (ChannelSftp) channel;
 
         ArrayList<File> filesToAdd = new ArrayList<File>();
-        File folder = new File("C:\\Users\\janru\\PycharmProjects\\TestCodeForCodeBubblesAR");
+        File folder = new File(this.ABSOLUTPATHTOPYCHARMPROJECTS + "\\" + this.PROJECTNAME);
         File[] listOfFiles = folder.listFiles();
         // Add files to be archived into zip file
         for (File file : listOfFiles) {
             filesToAdd.add(file);
         }
 
-        channelSftp.put(new FileInputStream("C:\\Users\\janru\\PycharmProjects\\TestCodeForCodeBubblesAR.zip"), "/home/jnruppenthal/Desktop/TestCodeForCodeBubblesAR.zip", ChannelSftp.OVERWRITE);
+        channelSftp.put(new FileInputStream(this.ABSOLUTPATHTOPYCHARMPROJECTS + "\\" + this.ZIPFILENAME), this.DESTINATION + this.ZIPFILENAME, ChannelSftp.OVERWRITE);
 
         session.disconnect();
         channelSftp.disconnect();
@@ -141,13 +154,11 @@ public class Main
         session.connect();
 
         StringBuilder command = new StringBuilder()
-                .append("cd Desktop")
+                .append("unzip -u " + this.DESTINATION + this.ZIPFILENAME)
                 .append(" ; ")
-                .append("unzip -u /home/jnruppenthal/Desktop/TestCodeForCodeBubblesAR.zip")
+                .append("rm " + this.DESTINATION + this.ZIPFILENAME)
                 .append(" ; ")
-                .append("rm /home/jnruppenthal/Desktop/TestCodeForCodeBubblesAR.zip")
-                .append(" ; ")
-                .append("python /home/jnruppenthal/Desktop/TestCodeForCodeBubblesAR/classMain.py");
+                .append("python " + this.DESTINATION + this.PROJECTNAME + "/classMain.py");
 
         ChannelExec channelExec = (ChannelExec) session.openChannel("exec");
         channelExec.setCommand(command.toString());
